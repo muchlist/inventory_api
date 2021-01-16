d626078288f6310ccdc280299659e02aeeda364d
muchlist <whois.muchlis@gmail.com>
2021-01-16 08:42:28 +0700

    check obj query

--- dao/check_obj_query.py
+++ dao/check_obj_query.py
@@ -13,59 +13,27 @@ _LAST_STATUS = "last_status"
 _NOTE = "note"
 
 
-def get_master_check(computer_id: str) -> dict:
+def get_check_obj(check_id: str) -> dict:
     find_filter = {
-        '_id': ObjectId(computer_id),
+        '_id': ObjectId(check_id),
     }
-    return mongo.db.computer.find_one(find_filter)
+    return mongo.db.check_obj.find_one(find_filter)
 
 
-def find_computer_by_branch_ip_clientname(branch: str,
-                                          ip_address: str,
-                                          client_name: str,
-                                          location: str,
-                                          division: str,
-                                          seat: str,
-                                          deactive: str) -> list:
+def find_check_obj(branch: str,
+                   name: str,
+                   obj_type: str, ) -> list:
     find_filter = {}
     if branch:
-        find_filter["branch"] = branch.upper()
-    if location:
-        find_filter["location"] = location.upper()
-    if division:
-        find_filter["division"] = division.upper()
-    if deactive:
-        if deactive == "yes":
-            find_filter["deactive"] = True
-        elif deactive == "no":
-            find_filter["deactive"] = False
-    if seat:
-        if seat == "yes":
-            find_filter["seat_management"] = True
-        elif seat == "no":
-            find_filter["seat_management"] = False
-    if ip_address:
-        find_filter["ip_address"] = ip_address
-    if client_name:
-        find_filter["client_name"] = {'$regex': f'.*{client_name.upper()}.*'}
+        find_filter[_BRANCH] = branch.upper()
+    if name:
+        find_filter[_NAME] = name.upper()
+    if obj_type:
+        find_filter[_TYPE] = obj_type.upper()
 
-    visible_filter = {
-        "_id": 1,
-        "branch": 1,
-        "client_name": 1,
-        "location": 1,
-        "division": 1,
-        "seat_management": 1,
-        "last_status": 1,
-        "ip_address": 1,
-        "case": 1,
-        "case_size": 1,
-    }
+    check_coll = mongo.db.check_obj.find(find_filter).sort((_TYPE, -1))
+    checks = []
+    for check in check_coll:
+        checks.append(check)
 
-    computer_coll = mongo.db.computer.find(find_filter, visible_filter).sort(
-        [("case_size", -1), ("location", 1), ("division", 1)])
-    computers = []
-    for computer in computer_coll:
-        computers.append(computer)
-
-    return computers
+    return checks
